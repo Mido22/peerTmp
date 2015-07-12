d2192fc2f616ae420e4548e1863ebbdb82ccdb23
mido22 <ban.mido22@gmail.com>
2015-07-13 02:06:20 +0800

    4

--- modules/database.js
+++ modules/database.js
@@ -1,23 +1,37 @@
 var redis = require('then-redis').createClient(),
-	path = require('path');  
+	path = require('path'),
+  EXPIRY_TIME = 15*60 // expire key in 15 minutes
+  ;  
 
 
 function incrKey(key){
-	redis.incr('usage:'+key);
+	redis.incr('usage:'+key).then(function(value){
+    console.log('incr key : ', value);
+    return value;
+  });
 }
 
 function set(key, val){
-	redis.set(key, val);
+	redis.set(key, val).then(function(value){
+    console.log('set key : ', value);
+    return value;
+  });
 }
 
 function get(key){
-	return redis.get(key);
+	return redis.get(key).then(function(value){
+    console.log('got key : ', value);
+    return value;
+  });
 }
 
 function del(key){
 	redis.del(key);
 }
 
+function removeExpiredTokens(user){
+
+}
 
 
 module.exports = {
