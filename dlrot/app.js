eb98ae8acf648fbc4419c4885e6d2eae39293037
mido22 <ban.mido22@gmail.com>
2015-11-11 23:44:51 +0800

    test

--- dlrot/app.js
+++ dlrot/app.js
@@ -5,7 +5,11 @@ var Browser = require('zombie')
   , divWord = '?title=[kat.cr]'
   , saveLocation='dl/'
   , selector = 'a[data-download]'
-  , url = 'https://kat.cr/user/YIFY/uploads/'
+  //, url = 'http://kat.cr/user/YIFY/uploads/?page='
+  , url = 'http://kickass.unblocked.la/user/YIFY/uploads/?page='
+  , promise = Promise.resolve()
+  , i = 1
+  , max=3
 ;
 
 function save(node){
@@ -22,29 +26,30 @@ function save(node){
 
 function visit(url){
   return new Promise( (r, re) => {
+    console.log('trying: ', url);
     browser.visit(url, err =>{
       if(err) return re(err);
       r();
     });
-  });
+  }).then(onSite);
 }
 
-function fe(e){
-  console.error(e);
+function fe(e){ console.error(e); }
+
+function fs(s){ console.log('all done...'); process.exit(); }
+
+function onSite(){ return Promise.all(browser.body.querySelectorAll(selector)._toArray().map(save));  }
+
+function chain(i){
+  promise = promise 
+    .then(() => console.log('trying page no: ', i))
+    .then(() => {return visit(url+i)})
+    .then(() => console.log('finished ', i, 'out of ', max-1, ' ...'))
+    .then(() => t(2000))
+  ;  
 }
 
-function fs(s){
-  console.log(s);
-}
+function t(s){return new Promise(r=>setTimeout(r, s || 5000))}
 
-function onSite(s){
-  console.log("page loaded... ");
-  var promises = browser.body.querySelectorAll(selector)._toArray().map(save);  
-  Promise.all(promises).catch(fe).then(()=> {
-    console.log('done... ');
-    process.exit();
-  });
-}
-
-
-visit(url).then(onSite).catch(fe);
+while(i<max)  chain(i++);
+promise.catch(fe).then(fs);
