749dd2889b27f4c195b7d28be16856e94e267b07
mido22 <ban.mido22@gmail.com>
2015-11-23 21:33:55 +0800

    test2

--- classicCrawl/app.js
+++ classicCrawl/app.js
@@ -1,33 +1,78 @@
-var Crawler = require("crawler");
+var Crawler = require("crawler")
+  , Datastore = require('nedb')
+  , host ='https://en.wikipedia.org'
+  , maxLevel = 5
+  , dbFile = './db.json'
+  , db = new Datastore({filename: dbFile})
+  , crawler
+  , count = 0
+  , pageCount = 0
+;
+
+db.loadDatabase(e=> console.log('db started...', e));
+
+function insert(link, name, nextTime){
+  db.update({_id:link}, {_id:link, name: name, next: !!nextTime}, {upsert: true}, e => {
+    if(e) return console.error(e);
+    count++;
+    console.log(name, 'data count: ', count);
+  });
+}
+
+
+
 //https://github.com/sylvinus/node-crawler
 
-var c = new Crawler({
-  maxConnections : 31,
-  rateLimits : 31,
-  skipDuplicates: true,
-  // This will be called for each crawled page
-  callback : function (error, result, $) {
-    // $ is Cheerio by default
-    //a lean implementation of core jQuery designed specifically for the server
-    /*$('a').each(function(index, a) {
-      var toQueueUrl = $(a).attr('href');
-      c.queue(toQueueUrl);
-    });*/
-    console.log('got results...');
-    var a=[];
-    $('a').each((i, v)=> a.push(v));
-    console.log(a.slice(0, 4));
-    process.exit(0);
-  }
+crawler = new Crawler({
+  maxConnections : 13,
+  rateLimits : 13,
+  skipDuplicates: true
 });
 
 function enQueue(link, level){
-  c.queue([{
-    uri: link,
+  if(level>maxLevel)  return insert(link, undefined, true);
+  crawler.queue([{
+    uri: host+link,
     callback: (err, res, $)=>{
-      
+      pageCount+=1;
+      console.log('pageCount: ', pageCount);
+      level = level+1;
+      $(a).each((i, v)=>{
+        switch(classifyLink(v.attribs.href)){
+          case 'file': insert(v.attribs.href, v.attribs.name);break;
+          case 'next': enQueue(v.attribs.href, level);break;
+          case 'fileLink': enQueueFileLink(v.attribs.href);break;
+          default: break;
+        }  
+      });      
     }
   }]);
 }
 
-c.queue('https://en.wikipedia.org/wiki/Catalogues_of_classical_compositions');
+function enQueueFileLink(link){
+  crawler.queue([{
+    uri: host+link,
+    callback: (err, res, $)=>{
+      pageCount+=1;
+      console.log('pageCount: ', pageCount);
+      $(a).each((i, v)=>{
+        if(classifyLink(v.attribs.href)==='file')  insert(v.attribs.href, v.attribs.name);
+      });
+    }
+  }]);  
+}
+
+function classifyLink(link){
+  if(!link || !link.indexOf('#'))  return 'ignore';
+  if(!link.indexOf('//upload.wikimedia.org')){
+    var ext = /\.([^\.]*)$/.exec(link)[1].toLowerCase();
+    if(!ext)  return 'ignore';
+    if(['ogg', 'oga', 'mp3', 'wav', 'm4a', 'opus', 'aac', 'wma', 'aiff', 'webm'].indexOf(ext)>-1) return 'file';
+    return 'ignore';
+  }
+  if(link.indexOf('File:')>-1)  return 'fileLink';
+  if(!link.indexOf('/wiki/')) return 'next';
+  return 'ignore';
+}
+
+enQueue('/wiki/Catalogues_of_classical_compositions', 0);
